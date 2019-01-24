9b202e359efa4620f64ca66ff9e730e29de88feb
clara <clarisaharguindey@gmail.com>
2019-01-24 15:28:46 +0100

    initial commit

---
+++ docs/precache-manifest.35f3f099153559b52e4a7a6c48cb6862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0d0664c7f967f18cf62c",
+    "url": "./static/js/main.0d0664c7.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "./static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "0d0664c7f967f18cf62c",
+    "url": "./static/css/main.3dbcbc92.chunk.css"
+  },
+  {
+    "revision": "d502c0d64ae017845336d1e7f3c78f39",
+    "url": "./index.html"
+  }
+];
